176883514667e084b3e2169e924cbe5aca54b231
alexgalex98 <73701449+alexgalex98@users.noreply.github.com>
2021-04-13 17:47:57 +0300

    resize photo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-//import CardList from './CardList'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
 //import {robots} from './robots'
